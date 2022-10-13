e5e7f0a55c9c1475e1b15746f835f0e8eec6e6af
dkolek <dkolek@dkolek.csad.cz>
2022-10-13 09:47:29 +0200

    simple API call + result shown on a page

--- src/main/java/cz/dkolek/cryptowatch/controller/MainController.java
+++ src/main/java/cz/dkolek/cryptowatch/controller/MainController.java
@@ -1,42 +1,34 @@
 package cz.dkolek.cryptowatch.controller;
 
-import cz.dkolek.cryptowatch.dto.CoinApiDTO;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.client.RestTemplate;
-
-import javax.servlet.http.HttpServletResponse;
-import java.net.URI;
-import java.net.URISyntaxException;
 
 @Controller
 public class MainController {
 
    @GetMapping("/index")
-   public String index() {
+   public String index(Model model) {
+      String data = apiCall();
+      model.addAttribute("data", data);
       return "index";
    }
 
-   @GetMapping(value = "/apiTest")
-   public void apiTest() throws URISyntaxException {
+   private String apiCall() {
       RestTemplate restTemplate = new RestTemplate();
 
       final String url = "https://rest.coinapi.io/v1/exchangerate/BTC/USD";
-      URI uri = new URI(url);
-
       HttpHeaders headers = new HttpHeaders();
       headers.set("X-CoinAPI-Key", "A949D348-58A4-4D25-ABDF-34E871FFE30D");
-
       HttpEntity<Void> requestEntity = new HttpEntity<>(headers);
 
       ResponseEntity<String> response = restTemplate.exchange(
               url, HttpMethod.GET, requestEntity, String.class);
-      System.out.println(response);
+      return response.getBody();
    }
 }
